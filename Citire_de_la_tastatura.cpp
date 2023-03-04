b6c19f7804720b5e74cd1b1d2a347470c1704fdd
Solomon Lidia <126985609+lidiasolomon@users.noreply.github.com>
2023-03-04 23:21:21 +0200

    Add files via upload

---
+++ Citire_de_la_tastatura.cpp
@@ -0,0 +1,11 @@
+﻿Build started...
+1>------ Build started: Project: Citire_de_la_tastatura, Configuration: Debug x64 ------
+1>pch.cpp
+1>AssemblyInfo.cpp
+1>Citire_de_la_tastatura.cpp
+1>Generating Code...
+1>.NETFramework,Version=v4.7.2.AssemblyAttributes.cpp
+1>AssemblyInfo.obj : MSIL module encountered; incremental linking is disabled for MSIL; performing full link
+1>Citire_de_la_tastatura.vcxproj -> C:\Users\SOLOMON\Desktop\Citire_de_la_tastatura\x64\Debug\Citire_de_la_tastatura.exe
+========== Build: 1 succeeded, 0 failed, 0 up-to-date, 0 skipped ==========
+========== Elapsed 00:06.743 ==========
